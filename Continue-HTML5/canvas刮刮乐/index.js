f526686612e0298ff06d01799d5bcf09f9eb275f
sgq <15054855725@163.com>
2020-02-14 17:19:45 +0800

    canvas刮刮乐

--- Continue-HTML5/canvas刮刮乐/index.js
+++ Continue-HTML5/canvas刮刮乐/index.js
@@ -1,43 +1,112 @@
-/**使用js来绘制图像 */
-var isdown = false,
-cover = document.getElementById('cover') // 首先找到canvas元素
-covercanvas = cover.getContext('2d') // 然后,创建context对象
+/**
+ * 项目思路：
+ * 首先将背景图片添加至canvas区域
+ * 上面加上一个遮罩层
+ * 需要在图片加载完成之后才能触发一系列的函数
+ * 由于在快速移动过程中 move事件触发不能完整的刮开区域 所以需要将没有刮开的区域用直线连接
+ * 
+ * 鼠标落下
+ * 获取当前坐标(旧坐标)
+ * 触发move up 事件
+ * 新旧像素合成问题转换
+ * 
+ * 鼠标移动
+ * 获取当前坐标(新坐标)
+ * 开始一个新的绘图区域
+ * 画出圆 进行滑动绘制
+ * 将现在的坐标赋值给旧的坐标
+ * 
+ * 鼠标抬起
+ * 清除move 和 up 事件
+ * 判断刮开的区域大小
+ * 
+ * 如果刮开区域大于70% 自动将画布清除 显示背景
+ * 通过获取当前canvas区域的像素值 比例比较
+ */
 
-// 下面两行代码绘制一个灰色的矩形
-// 设置fillStyle属性可以是css颜色，渐变,或图案 fillStyle 默认设置是#000000(黑色)
-covercanvas.fillStyle='transparent'
-covercanvas.fillRect(0,0,400,200)
+ var myCanvas = document.getElementById('myCanvas')
+      ctx = myCanvas.getContext('2d')
+      w = myCanvas.width
+      h = myCanvas.height
+      lastPoint = {}
+      nowPoint = {}
 
-// canvas 是一个二维网络
-// canvans 的左上角坐标为(0,0)
+function init() {
+  // 创建遮罩层
+  ctx.fillStyle = '#ccc'
+  ctx.fillRect(0,0,w,h)
 
-// 上面的fillRect 方法拥有参数(0,0,400,200)
-// 意思是: 在画布上绘制一个400 * 200的矩形,从左上角开始(0,0)
+  ctx.globalCompositeOperation = 'destination-out';
 
-function fillter(canvas) {
-  canvas.fillStyle = '#ccc'
-  canvas.fillRect(0,0,400,200)
-}
+  // 创建背景图片
+  var img = new Image()
+  img.src = './1.png'
+  img.onload = function() {
+    myCanvas.style.backgroundImage = 'url('+img.src+')'
 
-function isDown(e) {
-  e.pareventDefault()
-  isdown = true
-}
-
-function isUp(e) {
-  isdown = false
-}
-
-function draw(e) {
-  e.pareventDefault()
-  if(isdown) {
-    if(e.changedTouches) {
-      e = e.changedTouches[e.changedTouches.length - 1]
-    }
-    var _height = parseInt((window.innerHeight - 400) / 2),
-    _width = parseInt((window.innerWidth - 400) / 2),
-    touchTop = e.clientY - _height,
-    touchLeft = e.clientX - _width
-    wid
+    myCanvas.addEventListener('mousedown',downFunc,false)
   }
+}  
+
+init()
+
+// 鼠标按下事件
+function downFunc(e) {
+  lastPoint.x = e.clientX - myCanvas.offsetLeft
+  lastPoint.y = e.clientY - myCanvas.offsetTop
+
+  myCanvas.addEventListener('mousemove', moveFunc, false)
+  myCanvas.addEventListener('mouseup', upFunc, false)
+}
+
+// 鼠标移动事件
+function moveFunc(e) {
+  nowPoint.x = e.clientX - myCanvas.offsetLeft
+  nowPoint.y = e.clientY - myCanvas.offsetTop
+
+  ctx.beginPath()
+  // 将上次的坐标点与新坐标点连线
+  ctx.moveTo(lastPoint.x,lastPoint.y)
+  ctx.lineTo(nowPoint.x,nowPoint.y)
+  ctx.lineWidth = 40
+  ctx.lineCap = 'round'
+  ctx.stroke()
+
+  ctx.fillStyle = 'red'
+
+  // 绘制一个圆
+  ctx.arc(nowPoint.x,nowPoint.y,20,0,Math.PI*2,0)
+  ctx.closePath()
+  ctx.fill()
+
+  // 重新定义旧的坐标点
+  lastPoint.x = nowPoint.x
+  lastPoint.y = nowPoint.y
+}
+
+// 鼠标抬起事件
+
+function upFunc() {
+  myCanvas.removeEventListener('mousemove',moveFunc,false)
+  myCanvas.removeEventListener('mouseup',upFunc,false)
+
+  clearCanvas()
+}
+
+// 清除画布
+// 如果刮开区域大于70% 自动清除
+function clearCanvas() {
+  var d = ctx.getImageData(0,0,w,h)
+      // console.log(d)
+      c = 0
+      len = d.data.length
+
+      for(let i = 0;i < len; i+= 4) {
+        if(d.data[i] === 0) {
+          c++
+        }
+      }
+      if(c > len /4 * 0.7) {
+        ctx.clearRect(0,0,w,h)
+      }
 }
